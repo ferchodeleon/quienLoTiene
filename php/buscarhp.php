845d6e321ffd98a4f6ba03d8204e81b7519e3676
Fernando De Leon Gil <30879564+ferchodeleon@users.noreply.github.com>
2020-04-02 18:41:13 -0500

    Add to file php
    Add to file php

---
+++ php/buscarhp.php
@@ -0,0 +1,34 @@
+<?php
+	
+	include 'conexion.php';
+
+	$cedula = $_POST['cc'];
+	$fecnac = $_POST['fn'];
+	$mensaje = "";
+
+	$consulta = "SELECT * FROM base WHERE cedulaPersona = '$cedula' and fechaNacimiento = '$fecnac'";
+	$resultado = mysqli_query($conexion,$consulta);
+
+
+	$filas = mysqli_num_rows($resultado);
+
+	if($filas>0){
+
+ 	echo  $mensaje = '<script>
+   	 alert("Felicidades tu documentos se ha encontrado")
+   	 window.location.replace("buscar2.php")
+	</script>';
+
+	}else{
+
+	echo $mensaje = '<script>
+   	 alert("Lo sentimos no se ha encontrado tu documento")
+   	 window.location.replace("buscar.php")
+	</script>';
+
+	}
+	mysqli_free_result($resultado);
+	mysqli_close($conexion);
+
+
+?>
